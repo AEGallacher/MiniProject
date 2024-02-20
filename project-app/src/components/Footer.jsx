a3cbece57dfabecd54cdf23286edb01e86f40390
AEGallacher <amy.gallacher@btinternet.com>
2024-02-20 17:18:40 +0000

    Adding wave animation to footer element.

--- project-app/src/components/Footer.jsx
+++ project-app/src/components/Footer.jsx
@@ -1,13 +1,19 @@
-
+import '../styles/footer.css';
 
 function Footer(){
     
     return (
         <footer>
+            <div class="waves">
+                <div class="wave" id="wave1"></div>
+                <div class="wave" id="wave2"></div>
+                <div class="wave" id="wave3"></div>
+                <div class="wave" id="wave4"></div>
+            </div>
             <p>Test?</p>
         </footer>
     )
 
-}
+} 
 
 export default Footer
